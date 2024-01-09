cab5d80ea88ff05fa007a0ab47eea1d122846057
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2024-01-09 14:56:24 +0300

    #add #frontend - header navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,21 @@
 import React from "react"
 import s from "./Header.module.scss"
 import classNames from "classnames"
-import Link from "next/link"
+import HeaderNavigation from "./components/HeaderNavigation/HeaderNavigation"
 import Container from "@/common/MainContainer/MainContainer"
+
+const navLinks = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Blog", href: "/blog" },
+]
 
 const Header = () => {
   return (
     <header className={classNames(s["header"])}>
       <Container>
         <p>Header</p>
-        <ul className={classNames(s["nav"])}>
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/about"}>About</Link>
-          </li>
-          <li>
-            <Link href={"/blog"}>Blog</Link>
-          </li>
-        </ul>
+        <HeaderNavigation navLinks={navLinks} />
       </Container>
     </header>
   )
