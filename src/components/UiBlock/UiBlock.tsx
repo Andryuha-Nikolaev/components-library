7310a60b564ab2554de11b23dcd961ddbbec2dda
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2024-01-15 18:52:40 +0300

    #add #frontend - root button

--- src/components/UiBlock/UiBlock.tsx
+++ src/components/UiBlock/UiBlock.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 import s from "./UiBlock.module.scss"
 import RootButton from "@/ui/buttons/RootButton/RootButton"
@@ -5,7 +7,45 @@ import RootButton from "@/ui/buttons/RootButton/RootButton"
 const UiBlock = () => {
   return (
     <div className={s["block"]}>
-      <RootButton>Кнопка вариант 1</RootButton>
+      <RootButton textTransform="uppercase">Кнопка вариант 1</RootButton>
+      <RootButton styleVariant="2">Кнопка вариант 2</RootButton>
+      <RootButton buttonPosition="right">Кнопка позиция право</RootButton>
+      <RootButton buttonPosition="left" styleVariant="2">
+        Кнопка позиция лево
+      </RootButton>
+      <RootButton link="https://ya.ru/" role="link">
+        Кнопка как обычная ссылка
+      </RootButton>
+      <p>
+        текст{" "}
+        <RootButton template="link" link="https://ya.ru/" role="link">
+          ссылка как ссылка в тексте
+        </RootButton>
+        текст
+      </p>
+      <RootButton
+        styleVariant="2"
+        template="link"
+        link="/blog"
+        role="next-link"
+      >
+        ссылка некст
+      </RootButton>
+      <RootButton isDisabled>заблоченная кнопка</RootButton>
+      <RootButton isDisabled template="link" link="https://ya.ru/" role="link">
+        заблоченная ссылка
+      </RootButton>
+      <RootButton
+        onClick={() => {
+          console.log("aaaaaaaa")
+        }}
+        withArrow
+      >
+        Кнопка с логом
+      </RootButton>
+      <RootButton withArrow template="link" role="link" link="https://ya.ru/">
+        Со стрелкой
+      </RootButton>
     </div>
   )
 }
--- src/ui/buttons/RootButton/RootButton.tsx
+++ src/ui/buttons/RootButton/RootButton.tsx
@@ -1,17 +1,84 @@
 import React from "react"
 import s from "./RootButton.module.scss"
 import classNames from "classnames"
+import Link from "next/link"
+import ArrowIcon from "@/ui/icons/ArrowIcon/ArrowIcon"
 
 type RootButtonProps = {
   children: React.ReactNode
-  variant?: "1" | "2" | "3" | "4"
+  isDisabled?: boolean
+  onClick?: () => void
+  withArrow?: boolean
+  template?: "button" | "link"
+  styleVariant?: "1" | "2" | "3" | "4"
+  buttonPosition?: "left" | "center" | "right"
+  textTransform?: "none" | "uppercase" | "lowercase"
+  buttonType?: "button" | "submit"
+  role?: "button" | "next-link" | "link"
+  linkTarget?: "_blank" | "_self"
+  link?: string
 }
 
-const RootButton = ({ children, variant = "1" }: RootButtonProps) => {
+const RootButton = ({
+  children,
+  isDisabled = false,
+  onClick,
+  withArrow = false,
+  template = "button",
+  styleVariant = "1",
+  buttonPosition = "center",
+  textTransform = "none",
+  buttonType = "button",
+  role = "button",
+  linkTarget = "_self",
+  link,
+}: RootButtonProps) => {
+  const buttonClassName = classNames(
+    s["root"],
+    s[template],
+    s[`var${styleVariant}`],
+    template === "button" && s[buttonPosition],
+    isDisabled && s["disabled"]
+  )
+
+  const buttonStyle = {
+    textTransform: textTransform,
+  }
+
   return (
-    <button className={classNames(s["button"], s[`var${variant}`])}>
-      {children}
-    </button>
+    <>
+      {role === "button" && (
+        <button
+          onClick={onClick}
+          style={buttonStyle}
+          className={buttonClassName}
+          type={buttonType}
+        >
+          {children}
+        </button>
+      )}
+      {role === "link" && (
+        <a
+          style={buttonStyle}
+          className={buttonClassName}
+          href={link}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          {children} {withArrow && <ArrowIcon />}
+        </a>
+      )}
+      {role === "next-link" && (
+        <Link
+          style={buttonStyle}
+          className={buttonClassName}
+          href={link ? link : ""}
+          target={linkTarget}
+        >
+          {children} {withArrow && <ArrowIcon />}
+        </Link>
+      )}
+    </>
   )
 }
 
