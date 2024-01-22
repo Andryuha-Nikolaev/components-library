6325ef651638f045e78a86f6ad580864cacd1f43
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2024-01-22 16:33:19 +0300

    #add #frontend - burger icon

--- src/ui/icons/BurgerIcon/BurgerIcon.tsx
+++ src/ui/icons/BurgerIcon/BurgerIcon.tsx
@@ -10,15 +10,9 @@ const BurgerIcon = ({ isOpen }: BurgerIconProps) => {
   return (
     <div className={classNames(s["wrapper"])}>
       <div className={classNames(s["icon"], isOpen && s["open"])}>
-        <div className={classNames(s["line-wrap"])}>
-          <span className={classNames(s["line"])}></span>
-        </div>
-        <div className={classNames(s["line-wrap"])}>
-          <span className={classNames(s["line"])}></span>
-        </div>
-        <div className={classNames(s["line-wrap"])}>
-          <span className={classNames(s["line"])}></span>
-        </div>
+        <span className={classNames(s["line"])}></span>
+        <span className={classNames(s["line"])}></span>
+        <span className={classNames(s["line"])}></span>
       </div>
     </div>
   )
