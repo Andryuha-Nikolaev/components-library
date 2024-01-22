55c5afd35831168ad7daa9f6eb42840b119009e9
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2024-01-22 12:40:13 +0300

    #add #frontend - scrollbar style

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "",
+    //     port: "",
+    //     pathname: "/**",
+    //   },
+    // ],
   },
   experimental: {
     scrollRestoration: true,
